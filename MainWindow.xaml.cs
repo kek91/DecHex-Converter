5e6061f5472bb1205aac760b08d43cd0773c6821
Kim Eirik Kvassheim <kek91@users.noreply.github.com>
2020-04-14 22:21:35 +0200

    Add project files.

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,148 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Text.RegularExpressions;
+using System.Diagnostics;
+using System.Configuration;
+
+namespace DecHex_Converter
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            var appSettings = ConfigurationManager.AppSettings;
+
+            if (appSettings["AlwaysOnTop"] == "true")
+            {
+                this.Topmost = true;
+            }
+            else
+            {
+                this.Topmost = false;
+            }
+        }
+
+        public void UpdateSettings()
+        {
+            Debug.WriteLine("UpdateSettings()...");
+            var appSettings = ConfigurationManager.AppSettings;
+            if (appSettings["AlwaysOnTop"] == "true")
+            {
+                this.Activate();
+                this.Topmost = true;  // important
+                this.Topmost = false; // important
+                this.Focus();         // important
+                Debug.WriteLine("Set Topmost to true.");
+            }
+            else
+            {
+                this.Topmost = false;
+                Debug.WriteLine("Set Topmost to false.");
+            }
+            Debug.WriteLine("Fuck it... just restart app");
+
+            
+
+        }
+
+        /*
+         * private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            SettingWindow.Owner = this;
+        }
+        */
+
+        private void TextBox_Dec_KeyUp(object sender, KeyEventArgs e)
+        {
+            // Convert data to hex and update TextBox_Hex
+
+            //Debug.WriteLine(TextBox_Dec.Text);
+            string decValue = Regex.Replace(TextBox_Dec.Text, "[^0-9]", "");
+            
+            if(decValue.Length > 0)
+            {
+                string hexValue = int.Parse(decValue).ToString("X");
+                TextBox_Hex.Text = hexValue;
+            }
+            else
+            {
+                TextBox_Hex.Text = "";
+            }
+
+        }
+
+        private void TextBox_Hex_KeyUp(object sender, KeyEventArgs e)
+        {
+            // Convert data to dec and update TextBox_Dec
+
+            string hexValue = Regex.Replace(TextBox_Hex.Text, "[^abcdefABCDEF0-9]", "");
+            
+            if(hexValue.Length > 0)
+            {
+                string decValue = Convert.ToInt64(hexValue, 16).ToString();
+                TextBox_Dec.Text = decValue;
+            }
+            else
+            {
+                TextBox_Dec.Text = "";
+            }
+        }
+
+
+        private void MenuItem_FileSettings_Click(object sender, RoutedEventArgs e)
+        {
+            Window SettingWindow = new Settings();
+            SettingWindow.Show();
+        }
+
+        private void MenuItem_FileQuit_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBoxResult prompt = MessageBox.Show("Are you sure?",
+                "Quit",
+                MessageBoxButton.YesNo,
+                MessageBoxImage.Question,
+                MessageBoxResult.No);
+            if(prompt == MessageBoxResult.Yes)
+            {
+                Application.Current.Shutdown();
+            }
+        }
+
+        private void MenuItem_AboutVersion_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("v1.0.0\n2020-03-27",
+                "Version",
+                MessageBoxButton.OK,
+                MessageBoxImage.Information);
+        }
+
+        private void MenuItem_AboutCredits_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Made by Teknix - Kim Eirik Kvassheim\nⒸ 2020\nteknix.no",
+                "Credits",
+                MessageBoxButton.OK,
+                MessageBoxImage.Information);
+        }
+
+
+    }
+}
---
+++ Settings.xaml.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using System.Configuration;
+using System.Diagnostics;
+
+namespace DecHex_Converter
+{
+    /// <summary>
+    /// Interaction logic for Settings.xaml
+    /// </summary>
+    public partial class Settings : Window
+    {
+
+        public Settings()
+        {
+            InitializeComponent();
+
+            // Show correct checkmarks for checkboxes
+            try
+            {
+                var appSettings = ConfigurationManager.AppSettings;
+                if(appSettings["AlwaysOnTop"] == "true")
+                {
+                    CheckBox_AlwaysOnTop.IsChecked = true;
+                }
+                if (appSettings["EnableDarkTheme"] == "true")
+                {
+                    CheckBox_EnableDarkTheme.IsChecked = true;
+                }
+            }
+            catch (ConfigurationErrorsException)
+            {
+                Debug.WriteLine("Error reading app settings");
+            }
+        }
+
+        private void CheckBox_Click(object sender, RoutedEventArgs e)
+        {
+            // Toggle always on top
+            
+            try
+            {
+                var configFile = ConfigurationManager.OpenExeConfiguration(ConfigurationUserLevel.None);
+                var appSettings = configFile.AppSettings.Settings;
+
+                if (CheckBox_AlwaysOnTop.IsChecked == true)
+                {
+                    if (appSettings["AlwaysOnTop"] == null)
+                    {
+                        appSettings.Add("AlwaysOnTop", "true");
+                    }
+                    else
+                    {
+                        appSettings["AlwaysOnTop"].Value = "true";
+                    }
+                }
+                else
+                {
+                    if (appSettings["AlwaysOnTop"] == null)
+                    {
+                        appSettings.Add("AlwaysOnTop", "false");
+                    }
+                    else
+                    {
+                        appSettings["AlwaysOnTop"].Value = "false";
+                    }
+                }
+                
+                configFile.Save(ConfigurationSaveMode.Modified);
+                ConfigurationManager.RefreshSection(configFile.AppSettings.SectionInformation.Name);
+
+                MessageBox.Show("Setting has been successfully saved.\nPlease note that the changes will not be updated until you close and re-open DecHex Converter.",
+                    "Settings",
+                    MessageBoxButton.OK,
+                    MessageBoxImage.Information);
+            }
+            catch (ConfigurationErrorsException)
+            {
+                Debug.WriteLine("Error writing app settings");
+            }
+        }
+
+        private void CheckBox_Click_1(object sender, RoutedEventArgs e)
+        {
+            // Toggle dark theme
+            var appSettings = ConfigurationManager.AppSettings;
+            if (CheckBox_EnableDarkTheme.IsChecked == true)
+            {
+                appSettings["EnableDarkTheme"] = "true";
+                MessageBox.Show("Dark theme not implemented yet.",
+                    "Theme Settings",
+                    MessageBoxButton.OK,
+                    MessageBoxImage.Information);
+            }
+            else
+            {
+                appSettings["EnableDarkTheme"] = "false";
+            }
+        }
+    }
+}
